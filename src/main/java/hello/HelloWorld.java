27780afd56134588fef16c6df55b41f8e9611b22
direboar <minokuba@example.com>
2019-03-25 22:57:12 +0900

    ・dependenciesの定義追加 ・application pluginを追加（実行できるように） ・jarの定義を追加

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,7 +1,12 @@
 package hello;
 
+import java.time.LocalTime;
+
 public class HelloWorld {
 	public static void main(String[] args) {
+		LocalTime currentTime = LocalTime.now();
+		System.out.println("The current local time is: " + currentTime);
+
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
 	}
